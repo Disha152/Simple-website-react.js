813afe16ceee8fc1a45dd1b8378c99b5f779f5f0
Disha gupta <109867094+Disha152@users.noreply.github.com>
2023-07-21 23:09:27 +0530

    till brands added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import '../styles/home.scss';
 import '../styles/divider.scss';
 
-import imageSrc from '../assets/2.png'; // Replace this with the actual path to your image
+import imageSrc from '../assets/2.png'; 
+import{AiFillGoogleCircle,AiFillAmazonCircle,AiFillYoutube,AiFillInstagram} from "react-icons/ai";
 
 export default function Home() {
   return (
@@ -20,6 +21,61 @@ export default function Home() {
           We are your one and only soluiton to the tech problems you face everyday. We are leading tech companu whose aim is to increase the problem solving ability in children.
           </p></div>
       </div>
+      <div className="divider"/>
+    <div className='home3'>
+    <div>
+          <h1>Who are we?</h1>
+          <p>We are a group of tech enthusiasts who want to help you with your tech problems.
+            We are a group of tech enthusiasts who want to help you with your tech problems.
+            We are a group of tech enthusiasts who want to help you with your tech problems.
+            We are a group of tech enthusiasts who want to help you with your tech problems.
+            We are a group of tech enthusiasts who want to help you with your tech problems.
+          </p>
+        </div>
+      </div>
+     
+      <div className='home4'>
+
+        <h1>Brands</h1>
+        <div>
+     
+         <article>
+          <div style={{animationDelay :"0.35s"}}>
+            <AiFillGoogleCircle/>
+            <p>Google</p>
+            <br/>
+          </div>
+         </article>
+
+         <article>
+          <div style={{animationDelay :"0.55s"}}>
+            <AiFillAmazonCircle/>
+            <p>Amazon</p>
+            <br/>
+
+          </div>
+         </article>
+
+         <article>
+          <div style={{animationDelay :"0.75s"}}>
+            <AiFillYoutube/>
+            <p>Youtube</p>
+            <br/>
+
+          </div>
+         </article>
+
+         <article>
+          <div style={{animationDelay :"0.95s"}}>
+            <AiFillInstagram/>
+            <p>Instagram</p>
+            <br/>
+
+          </div>
+         </article>
+        </div>
+      </div>
+
     </>
   );
 }
